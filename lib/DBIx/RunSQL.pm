dc2df64cec67428299acf99bc6ce5afdc642d045
Max Maischein <corion@corion.net>
2018-02-13 15:42:19 +0100

    Bump copyright year

--- lib/DBIx/RunSQL.pm
+++ lib/DBIx/RunSQL.pm
@@ -653,7 +653,7 @@ Max Maischein C<corion@cpan.org>
 
 =head1 COPYRIGHT (c)
 
-Copyright 2009-2017 by Max Maischein C<corion@cpan.org>.
+Copyright 2009-2018 by Max Maischein C<corion@cpan.org>.
 
 =head1 LICENSE
 
